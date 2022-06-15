3ff94c0b5c3d6e6944285afe75b9fee221063718
Nile Okomo <okomonile3@gmail.com>
2022-06-15 20:48:26 +0300

    Try fix idea

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => `$${this.income}`,
-    getIncomeInEuros: () => `${this.income} euros`,
+    getIncomeInDollars: () => `$${income}`,
+    getIncomeInEuros: () => `${income} euros`,
   };
 
   return fullBudget;
