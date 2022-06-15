f584c92879675dccbfd9bd753188479255e604b7
Nile Okomo <okomonile3@gmail.com>
2022-06-15 22:29:19 +0300

    Test arrow function

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
+    getIncomeInDollars: () => `$${fullBudget.income}`,
+    getIncomeInEuros: () => `${fullBudget.income} euros`,
   };
 
   return fullBudget;
