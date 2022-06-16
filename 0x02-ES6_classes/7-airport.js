6beb2e1c437c4a08335ee14c288ab06f9e150e49
Nile Okomo <okomonile3@gmail.com>
2022-06-17 02:21:01 +0300

    Fix linter errors

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,19 +1,19 @@
 export default class Airport {
-    constructor(name, code) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        } else {
-            this._name = name;
-        }
-
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        } else {
-            this._code = code;
-        }
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
     }
 
-    toString() {
-        return `[object ${this._code}]`;
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = code;
     }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
