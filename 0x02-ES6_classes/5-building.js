a343cb1fa6cc5a27b252980137c3df58b427aa40
Nile Okomo <okomonile3@gmail.com>
2022-06-17 01:40:49 +0300

    Implement Abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+    constructor(sqft) {
+        if (typeof sqft !== 'number') {
+            throw new TypeError('Sqft must be a number');
+        } else {
+            this._sqft = sqft;
+        }
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
