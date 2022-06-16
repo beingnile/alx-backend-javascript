bfa8652c4154a9dad616f6538f97405374668701
Nile Okomo <okomonile3@gmail.com>
2022-06-16 22:04:53 +0300

    Update gitignore file

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
-    const idx = array.indexOf(value);
     const val = `${appendString}${value}`;
-    array[idx] = val;
+    array.setAttribute(value, val);
   }
 
   return array;
