83fd3d671de8c8788ba0b78095dc56e90f45d4ee
Nile Okomo <okomonile3@gmail.com>
2022-06-17 05:05:11 +0300

    Throw error if instance lacks mandatory method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -5,13 +5,13 @@ export default class Building {
     } else {
       this._sqft = sqft;
     }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
