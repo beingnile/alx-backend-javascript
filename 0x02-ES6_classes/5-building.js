2b01bb10e1ed50ef992b5558244bb1aca0d84b77
Nile Okomo <okomonile3@gmail.com>
2022-06-17 05:15:19 +0300

    Add condition for class extending Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -6,7 +6,7 @@ export default class Building {
       this._sqft = sqft;
     }
 
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
