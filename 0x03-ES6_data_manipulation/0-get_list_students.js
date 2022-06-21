93d2666520dcebef41152c1e3b5a228ccf84829a
Nile Okomo <okomonile3@gmail.com>
2022-06-22 00:04:19 +0300

    Fix linter errors

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  const myArray = new Array(
+  const myArray = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 3, firstName: 'Serena', location: 'San Francisco' },
-  );
+  ];
 
   return myArray;
 }
